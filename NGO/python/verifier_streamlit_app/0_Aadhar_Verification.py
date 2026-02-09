36600ccd92a587f3807ed2dafc1ab32f19c6ea52
Chaitanya Sannidhi <chaitusannidhi@gmail.com>
2026-02-09 23:57:38 +0530

    Refactor Aadhar verification logic and imports

--- NGO/python/verifier_streamlit_app/0_Aadhar_Verification.py
+++ NGO/python/verifier_streamlit_app/0_Aadhar_Verification.py
@@ -1,24 +1,16 @@
 import streamlit as st
-from ultralytics import YOLO
-from PIL import Image
 import cv2
-from supervision import Detections
 import pytesseract
 import numpy as np
+import json
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+from supervision import Detections
+from ultralytics import YOLO
+from PIL import Image
+from utils.ocr_utils import *
+from utils.s3_utils import *
 
-def enhance_for_ocr(img_bgr):
-    lab = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2LAB)
-    l, a, b = cv2.split(lab)
-
-    clahe = cv2.createCLAHE(clipLimit=2.5, tileGridSize=(4, 4))
-    l_enhanced = clahe.apply(l)
-
-    lab_enhanced = cv2.merge((l_enhanced, a, b))
-    enhanced = cv2.cvtColor(lab_enhanced, cv2.COLOR_LAB2BGR)
-
-    return enhanced
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 def extract_aadhar_content(aadhar_image):
     extracted_info = dict()
@@ -32,7 +24,6 @@ def extract_aadhar_content(aadhar_image):
 
     for i, xyxy in zip(class_list, xyxy_list):
         preprocessed_region = enhance_for_ocr(extract_region(aadhar_image, xyxy))
-        # _, preprocessed_region = cv2.threshold(preprocessed_region, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         extracted_text = pytesseract.image_to_string(preprocessed_region, config = custom_config).strip()
         print(f'{id2reg[i]} contains {extracted_text}')
         extracted_info[f'{id2reg[i]}'] = extracted_text
@@ -42,44 +33,6 @@ def extract_aadhar_content(aadhar_image):
 def extract_region(img, xyxy):
     x1, y1, x2, y2 = [int(coordinate) for coordinate in xyxy]
     return img[y1:y2, x1:x2]
-
-def field_validation(name, aadhar_num, dob, gender):
-    validated_info = dict()
-    
-    # Date of Birth
-    if len(dob.split('/')) != 3:
-        dob = None
-    validated_info['Date of Birth'] = dob
-    
-    # Aadhar Number
-    aadhar_num_parts = aadhar_number.split(' ')
-    if len(aadhar_num_parts) == 3:
-        for part in aadhar_num_parts:
-            if len(part) == 4:
-                continue
-            else:
-                aadhar_number = None
-                break
-    else:
-        aadhar_number = None
-    validated_info['Aadhar Number'] = aadhar_num
-    
-    # Name
-    for char in name:
-        if not (char.isalpha() or char.isspace()):
-            name = None
-            break
-    if name is not None:
-        if len(name) <= 3:
-            name = None
-    validated_info['Name'] = name
-    
-    #Gender
-    if gender.lower() not in ('male', 'female'):
-        gender = None
-    validated_info['Gender'] = gender
-    
-    return validated_info
     
 def create_aadhar_json(name, aadhar_num, dob, gender):
     return {
@@ -103,15 +56,60 @@ if "detection_model" not in st.session_state:
     
 if "extraction_model" not in st.session_state:
     st.session_state.extraction_model = YOLO("models/model.pt")
-
+    
 # Persist state
-if "verified_aadhar" not in st.session_state:
-    st.session_state.verified_aadhar = False
+st.session_state.status = {
+    'verified_aadhar': False,
+    'verified_face': False,
+    'verified_income': False,
+    'verified_rec': False,
+    'verified_marksheet': False,
+    'verified_mset': False
+}
+
+# if "verified_aadhar" not in st.session_state:
+#     st.session_state.verified_aadhar = False
 
 st.title("Aadhar Upload and Verification")
 
+aadhar = st.text_input(
+    "Enter 12-digit Aadhaar number (no spaces)",
+    max_chars=12
+)
+
+if st.button("Verify Aadhaar"):
+    if not aadhar.isdigit() or len(aadhar) != 12:
+        st.error("Please enter a valid 12-digit Aadhaar number.")
+    else:
+        if folder_exists(aadhar):
+            st.session_state.folder_name = aadhar
+            details_json = download_file_bytes(f'{st.session_state.folder_name}/details.json')
+            st.session_state.info_json = details_json
+            if 'Aadhar' in details_json and details_json['Aadhar']['Verified']:
+                st.session_state.status['verified_aadhar'] = True
+            if 'Photograph' in details_json and details_json['Photograph']['Verified']:
+                st.session_state.status['verified_face'] = True
+            if 'Income Certificate' in details_json and details_json['Income Certificate']['Verified']:
+                st.session_state.status['verified_income'] = True
+            if 'Recommendation Letter' in details_json and details_json['Recommendation Letter']['Verified']:
+                st.session_state.status['verified_rec'] = True
+            if 'Marksheet' in details_json and details_json['Marksheet']['Verified']:
+                st.session_state.status['verified_marksheet'] = True
+            if 'MSET Application' in details_json and details_json['MSET_Application']['Verified']:
+                st.session_state.status['verified_mset'] = True
+        else:
+            st.session_state.folder_name = aadhar
+            create_s3_folder(str(st.session_state.folder_name))
+            info_json = {
+                'Aadhar':
+                    {
+                        'Verified': 'False'
+                    }
+            }
+            upload_json(f'{st.session_state.folder_name}/details.json',info_json)
+
 # If Aadhaar was already verified before and user navigates back → DO NOT RESET
-if st.session_state.verified_aadhar:
+if st.session_state.status['verified_aadhar']:
     st.success("Aadhar already verified earlier")
     st.page_link("pages/1_Face_Verification.py", label="Continue", icon="▶")
     st.button("Reupload Aadhar Image", on_click=reset_state)
@@ -130,14 +128,13 @@ else:
         class_name = st.session_state.detection_model.names[cls_id]
 
         if class_name.lower() == "aadhar":
-            st.session_state.verified_aadhar = True 
             st.success(f"Aadhaar detected with Confidence: ({conf:.2f})")
             
             with st.spinner("Reading Aadhar Content..."):
                 image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
                 extracted_info = extract_aadhar_content(image)
                 
-            st.success("Details extracted successfully")
+            st.success("Details extracted")
             
             name = extracted_info.get('NAME',None)
             aadhar_number = extracted_info.get('AADHAR_NUMBER',None)
@@ -145,12 +142,18 @@ else:
             dob = extracted_info.get('DATE_OF_BIRTH', None)
             
             aadhar_json = create_aadhar_json(name, aadhar_number, gender, dob)
-            st.session_state.info_json = aadhar_json
             
-            st.write(f"Name: {name}")
-            st.write(f"Aadhar: {aadhar_number}")
-            st.write(f"Gender: {gender}")
-            st.write(f"DOB: {dob}")
+            st.write(aadhar_json)
+            
+            img = np.array(Image.open(uploaded))
+            image_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            
+            url = upload_cv2_image_to_s3(image_bgr, st.session_state.folder_name,'uploaded_aadhar.jpeg')
+            aadhar_json['Aadhar']['Image URL'] = url
+            st.session_state.info_json = aadhar_json
+            upload_json(f'{st.session_state.folder_name}/details.json',st.session_state.info_json)
+            
+            st.session_state.status['verified_aadhar'] = True
             
             st.page_link("pages/1_Face_Verification.py", label="Continue", icon="▶")
 
