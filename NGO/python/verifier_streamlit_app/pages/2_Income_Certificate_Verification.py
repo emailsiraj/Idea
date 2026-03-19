c11ff33713a8453b1fc2482a4364ee74222e6387
Chaitanya Sannidhi <chaitusannidhi@gmail.com>
2026-03-19 16:52:17 +0530

    Refactor income certificate verification logic

--- NGO/python/verifier_streamlit_app/pages/2_Income_Certificate_Verification.py
+++ NGO/python/verifier_streamlit_app/pages/2_Income_Certificate_Verification.py
@@ -9,7 +9,7 @@ from utils.img_utils import *
 from utils.ocr_utils import *
 from utils.s3_utils import *
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 REGION_CONFIG = {
     "title": {
